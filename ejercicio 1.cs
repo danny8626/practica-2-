f33462b7326686cd20cd31e6c80aa88f0cb3b1ff
Danny Galva <79226576+danny8626@users.noreply.github.com>
2021-03-15 10:20:26 -0400

    Add files via upload

---
+++ ejercicio 1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int num =2, total=0, suma=0;
+            while (num != 0)
+            {
+                Console.Write("Escribe un numero: ");
+                num = int.Parse(Console.ReadLine());
+                suma += num;
+                total += 1; 
+            }
+            Console.WriteLine();
+            Console.WriteLine("El total de numeros ingresados es: " + total);
+            Console.WriteLine("La suma de los numeros ingresados es: " + suma);
+            Console.WriteLine("Ya sali del Programa =( , Enter para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejercicio 3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ejercico_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //Ejercicio 3//
+            // Console.WriteLine("Programa que genera la tabla de multiplicar de los numeros del 1 al 100");
+
+            for (int i=1; i <= 500; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Tabla del {i}");
+                    Console.WriteLine();
+                    for (int num=1; num<=10; num++)
+                    {
+                       
+                        Console.WriteLine($"{num} x {i} = " + num * i);
+                    }
+                   
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+            
+
+
